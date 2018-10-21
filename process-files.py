36cb03758c041442a1683ab072191912610d3ddc
Philip May <eniak.info@gmail.com>
2018-10-21 22:27:16 +0200

    multiprocessing added

--- process-files.py
+++ process-files.py
@@ -1,9 +1,12 @@
 from somajo import Tokenizer, SentenceSplitter
 import os
+from multiprocessing import Pool, cpu_count
 
-INPUT_TEXT_DIR = "data"
+INPUT_DIR = "data"
 
-OUTPUT_FILE = "output.txt"
+OUTPUT_DIR = "output"
+
+PROCESS_DISCUSSION = False
 
 def is_doc_start_line(line):
     return line.startswith('<doc')
@@ -23,6 +26,13 @@ def remove_discussion_suffix(sentence):
 
     return sentence
 
+def get_data_dirs(root_dir):
+    result = []
+    for _, d_, _ in os.walk(root_dir):
+        for dir in d_:
+            result.append(dir)
+    return result
+
 def process_text_line(line):
     tokenizer = Tokenizer()
     tokens = tokenizer.tokenize(line)
@@ -33,61 +43,89 @@ def process_text_line(line):
     result = []
 
     for s in sentences:
-        s = remove_discussion_suffix(s)
 
+        if PROCESS_DISCUSSION:
+            s = remove_discussion_suffix(s)
 
         if len(s) >= 4:
             sentence_string = " ".join(s)
 
-            # check if this line still contains a dirty comment:
-            if "( CEST )" not in sentence_string and "( CET )" not in sentence_string:
+            if PROCESS_DISCUSSION:
+                # check if this line still contains a dirty comment:
+                if "( CEST )" not in sentence_string and "( CET )" not in sentence_string:
+                    result.append(sentence_string)
+            else:
                 result.append(sentence_string)
+
 
     return result
 
-with open(OUTPUT_FILE, 'a') as output_file:
+def process_directory(input_dir, output_file):
+    with open(os.path.join(OUTPUT_DIR, output_file), 'a') as output_file:
 
-    # to avoid new line at end of file
-    first_line_written = False
+        # to avoid new line at end of file
+        first_line_written = False
 
-    # r_=root, d_=directories, f_=files
-    for r_, d_, f_ in os.walk(INPUT_TEXT_DIR):
-        for file_ in f_:
-            next_input_file = os.path.join(r_, file_)
-            print("Reading file:", next_input_file)
+        # r_=root, d_=directories, f_=files
+        for r_, _, f_ in os.walk(input_dir):
+            for file_ in f_:
+                next_input_file = os.path.join(r_, file_)
+                print("Reading file:", next_input_file)
 
-            with open(next_input_file, "r") as input_file:
+                with open(next_input_file, "r") as input_file:
 
-                skip_next_line = False
-                
-                for line in input_file:
-
-                    # drop line with start tag
-                    if is_doc_start_line(line):
-                        skip_next_line = True
-                        continue
-
-                    # drop line with end tag
-                    if is_doc_end_line(line):
-                        continue
-
-                    # skip first line to skip headline
-                    if skip_next_line == True:
-                        skip_next_line = False
-                        continue
-
-                    # skip empty lines
-                    if len(line) <= 1:
-                        continue
+                    skip_next_line = False
                     
-                    sentences = process_text_line(line)
-                    
-                    # ignore blank lines and make sure that stuff like "\n" is also ignored:
-                    if len(sentences) > 2:
-                        for sentence in sentences:
-                            if first_line_written == True:
-                                output_file.write("\n")
-                            else:
-                                first_line_written = True
+                    for line in input_file:
 
-                            output_file.write(sentence)
+                        # drop line with start tag
+                        if is_doc_start_line(line):
+                            skip_next_line = True
+                            continue
+
+                        # drop line with end tag
+                        if is_doc_end_line(line):
+                            continue
+
+                        # skip first line to skip headline
+                        if skip_next_line == True:
+                            skip_next_line = False
+                            continue
+
+                        # skip empty lines
+                        if len(line) <= 1:
+                            continue
+                        
+                        sentences = process_text_line(line)
+                        
+                        # ignore blank lines and make sure that stuff like "\n" is also ignored:
+                        if len(sentences) > 2:
+                            for sentence in sentences:
+                                if first_line_written == True:
+                                    output_file.write("\n")
+                                else:
+                                    first_line_written = True
+
+                                output_file.write(sentence)
+
+def pd(map_item):
+    """Wrap call to process_directory to be called by map function"""
+    input_dir, output_file = map_item
+    print("Creating:", output_file)
+    process_directory(input_dir, output_file)
+
+if __name__ == '__main__':
+    data_dirs = get_data_dirs(INPUT_DIR)
+
+    call_list = []
+    for dir in data_dirs:
+        call_item = [os.path.join(INPUT_DIR, dir), dir + ".txt"]
+        call_list.append(call_item)
+
+    pool_size = cpu_count() * 2
+    print("pool_size:", pool_size)
+
+    with Pool(pool_size) as p:
+        p.map(pd, call_list)
+
+    print("Done!")
