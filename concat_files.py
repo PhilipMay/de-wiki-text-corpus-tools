6ef665e6ab53c29b8edfc56228dd76deb8695e5c
Philip May <eniak.info@gmail.com>
2018-10-24 08:41:42 +0200

    concat tool and covab file writer

---
+++ concat_files.py
@@ -0,0 +1,22 @@
+import os
+import shutil
+
+INPUT_DIR = 'data'
+
+TARGET_FILE = 'output.txt'
+
+first_file_written = False
+
+if __name__ == '__main__':
+    with open(TARGET_FILE, 'a') as output_file:
+        for root_dir, _, files in os.walk(INPUT_DIR):
+            for f in files:
+                next_file = os.path.join(root_dir, f)
+                print(next_file)
+                with open(next_file, "r") as input_file:
+                    if first_file_written == True:
+                        output_file.write("\n")
+                    else:
+                        first_file_written = True
+
+                    shutil.copyfileobj(input_file, output_file)
---
+++ vocab_file_writer.py
@@ -0,0 +1,30 @@
+INPUT_FILE = 'text_corpus.txt'
+
+OUTPUT_FILE = 'vocab.txt'
+
+vocab_dict = {}
+
+if __name__ == '__main__':
+    with open(INPUT_FILE, "r") as input_file: 
+        with open(OUTPUT_FILE, "w") as output_file:         
+            for line in input_file:
+                tokens = line.split()
+                for token in tokens:
+                    value = vocab_dict.get(token, 0)
+                    vocab_dict[token] = value + 1
+        
+            print(vocab_dict)
+            data = [(value, key) for key, value in vocab_dict.items()]
+            data.sort(reverse=True)
+            print(data)
+            output_file.write("<S>\n</S>\n<UNK>")
+            for d in data:
+                _, key = d
+                output_file.write("\n")
+                output_file.write(key)
+
+    # TODO: <S>, </S> and <UNK> on top of file
+
+    #data = [(100, "Hallo"), (10, "du"), (20, "ich")]
+    #data.sort()
+    #print(data)
