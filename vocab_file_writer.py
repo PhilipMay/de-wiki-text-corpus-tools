55803f76c768bfe40dce5c9fe8c2606dd424829e
Philip May <eniak.info@gmail.com>
2018-10-26 07:58:39 +0200

    print number of token in vocab writer

--- vocab_file_writer.py
+++ vocab_file_writer.py
@@ -4,24 +4,30 @@ OUTPUT_FILE = 'vocab.txt'
 
 vocab_dict = {}
 
+token_count = 0
+
 if __name__ == '__main__':
     with open(INPUT_FILE, "r") as input_file: 
         with open(OUTPUT_FILE, "w") as output_file:         
             for line in input_file:
                 tokens = line.split()
+                #print(tokens, "-", len(tokens))
+                token_count += len(tokens)
                 for token in tokens:
                     value = vocab_dict.get(token, 0)
                     vocab_dict[token] = value + 1
         
-            print(vocab_dict)
+            #print(vocab_dict)
             data = [(value, key) for key, value in vocab_dict.items()]
             data.sort(reverse=True)
-            print(data)
+            #print(data)
             output_file.write("<S>\n</S>\n<UNK>")
             for d in data:
                 _, key = d
                 output_file.write("\n")
                 output_file.write(key)
+
+    print("Number of tokens:", token_count)
 
     # TODO: <S>, </S> and <UNK> on top of file
 
