8c86b69ad1393ef5e118d26d6c1820f8c9cc479d
Philip May <eniak.info@gmail.com>
2018-10-31 22:18:35 +0100

    for concat & text corpus filter

--- concat_files.py
+++ concat_files.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 
-INPUT_DIR = 'data'
+INPUT_DIR = 'data/AA'
 
 TARGET_FILE = 'output.txt'
 
@@ -13,8 +13,9 @@ if __name__ == '__main__':
             for f in files:
                 next_file = os.path.join(root_dir, f)
                 print(next_file)
+                #first_file_written = False
                 with open(next_file, "r") as input_file:
-                    if first_file_written == True:
+                    if first_file_written:
                         output_file.write("\n")
                     else:
                         first_file_written = True
